e10cdc7ffc7f14fbfb0950600a51a4f0e1141bd8
A1b3rt0M3rcad0 <alberto.zimba2013@gmail.com>
2023-08-10 15:48:16 -0300

    third commit

--- transform_data.py
+++ transform_data.py
@@ -4,7 +4,7 @@ import numpy as np
 def transform_data(df, medias=[7, 14, 21, 200], drop_columns=['open', 'high', 'low', 'tick_volume', 'spread', 'real_volume', 'time']):
     ## Criando métricas
     # Cria a variação do dia anterior com o atual
-    df['close%'] = df['close'].shift(1)/df['close']
+    df['close%'] = df['close']/df['close'].shift(1)
     # Cria médias móveis
     for mm in medias:
         df[f'mm{mm}'] = df['close'].rolling(mm).mean()
